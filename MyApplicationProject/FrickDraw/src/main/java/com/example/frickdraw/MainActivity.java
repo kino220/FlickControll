ac4785e8f0a786243f5819c38947f6fc1ceb99f1
kino220 <m.j.1213.bad@gmail.com>
2013-08-26 15:51:57 +0900

    SDKのパスとエンコードエラーの修正
    エラーメッセージ
    Gradle: 警告:この文字は、エンコーディング MS932 にマップできません。
    
    修正方法
    build.gradleに以下のコードを加える
    
    tasks.withType(JavaCompile) {
    options.encoding = 'UTF-8'
    }

--- MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/MainActivity.java
+++ MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.frickdraw;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+
+        TextView text;
+        text = new TextView(this);
+        text.setText("てすてす");
+
+        mainLayout.addView(text);
+
+
     }
 
 
