e6f8cce0ca73cd8fb24df9d0408de27b7e9baa68
kino220 <m.j.1213.bad@gmail.com>
2013-10-01 20:28:00 +0900

    タッチで線が書けるよ！やったね！

---
+++ MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/GraphicView.java
@@ -0,0 +1,77 @@
+package com.example.frickdraw; /**
+ * Created by Iplab on 13/10/01.
+ */
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+
+
+public class GraphicView extends View {
+
+    private int initialPointX, initialPointY,endPointX,endPointY;
+
+    public GraphicView(Context context, int width, int height) {
+        super(context);
+
+
+
+        initialPointX = (int)(width/2);
+        initialPointY = (int)(height/2);
+        endPointX = initialPointX;
+        endPointY = initialPointY;
+
+    }
+
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        // 座標系がわかるような罫線を引く
+        Paint paint = new Paint();
+        paint.setColor(Color.argb(75, 255, 255, 255));
+        paint.setStrokeWidth(1);
+        for (int y = 0; y < 800; y = y + 10) {
+            canvas.drawLine(0, y, 479, y, paint);
+        }
+        for (int x = 0; x < 480; x = x + 10) {
+            canvas.drawLine(x, 0, x, 799, paint);
+        }
+
+        // 線を書く
+        paint.setColor(Color.RED);
+        // だんだん大きくしていく。
+        for (int i = 1; i <= 10; i++) {
+            paint.setStrokeWidth(i);
+            canvas.drawLine(i * 20 - 10, 10, i * 20 + 10, 10, paint);
+        }
+
+        paint.setColor(Color.rgb(0, 255, 0));
+        paint.setStrokeWidth(10);
+        canvas.drawLine(10, 40, 300, 70, paint);
+        paint.setStrokeWidth(10);
+        paint.setAntiAlias(true);
+        canvas.drawLine(10, 60, 300, 90, paint);
+
+        //フリックの軌跡描画はこの辺
+        canvas.drawLine(initialPointX, initialPointY, endPointX, endPointY, paint);
+
+    }
+
+    public void setInitialPointX(int initialPointX) {
+        this.initialPointX = initialPointX - 300;
+    }
+
+    public void setInitialPointY(int initialPointY) {
+        this.initialPointY = initialPointY - 300;
+    }
+
+    public void setEndPointX(int endPointX) {
+        this.endPointX =  initialPointX + endPointX ;
+    }
+
+    public void setEndPointY(int endPointY) {
+        this.endPointY = initialPointY + endPointY ;
+    }
+}
--- MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/MainActivity.java
+++ MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.frickdraw;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 
     float sx = 0, sy =0, tx = 0, ty =0;
     long startTime = 0, endTime = 0;
-    RelativeLayout mainLayout;
-    TextView text;
+    private RelativeLayout mainLayout;
+    private TextView text;
+    private GraphicView gview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +33,28 @@ public class MainActivity extends Activity {
 
 
 
+        //Textview初期化
         text = new TextView(this);
         String str = String.format("StartPoint:(%f,%f) \n" +
                 "TerminalPoint:(%f,%f)",sx,sy,tx,ty);
         text.setText(str);
 
+        //mainLayoutにTextViewを追加
         mainLayout.addView(text);
 
+        // ウィンドウマネージャのインスタンス取得
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        // ディスプレイのインスタンス生成
+        Display display = wm.getDefaultDisplay();
+
+        //GraphicView初期化
+        gview = new GraphicView(this, display.getWidth(), display.getHeight());
+
+        //mainLayoutにGraphicViewを追加
+        mainLayout.addView(gview);
+
+        //タッチリスナの設定
+        //タップをどのように認識するかの記述はこの辺
         mainLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent e) {
@@ -46,6 +65,10 @@ public class MainActivity extends Activity {
                     case MotionEvent.ACTION_DOWN:
                         sx = e.getX();
                         sy = e.getY();
+
+                        gview.setInitialPointX((int)sx);
+                        gview.setInitialPointY((int)sy);
+
                         Log.i("TouchTest","ActionDown");
                         motion = "Action_Down";
                         startTime = System.currentTimeMillis();
@@ -53,12 +76,22 @@ public class MainActivity extends Activity {
                     case MotionEvent.ACTION_MOVE:
                         tx = e.getX();
                         ty = e.getY();
+
+                        gview.setEndPointX((int)(tx - sx));
+                        gview.setEndPointY((int)(ty - sy));
+
+                        gview.invalidate();
                         Log.i("TouchTest","ActionMove");
                         motion = "Action_Move";
                         break;
                     case MotionEvent.ACTION_UP:
                         tx = e.getX();
                         ty = e.getY();
+
+                        gview.setEndPointX((int)(tx - sx));
+                        gview.setEndPointY((int)(ty - sy));
+
+                        gview.invalidate();
                         Log.i("TouchTest","ActionUp");
                         motion = "Action_Up";
                         endTime = System.currentTimeMillis();
@@ -73,42 +106,24 @@ public class MainActivity extends Activity {
                         "motion:"+motion ,sx,sy,tx,ty,(float)Math.sqrt( (double)((tx - sx)*(tx - sx) + (ty - sy)*(ty - sy)) ),(float)(endTime - startTime));
                 text.setText(str);
 
-                //mainLayout.invalidate();
 
                 return true;
             }
         });
 
 
+
+
     }
 
 
-
+/*
+//  メニューの記述はこの辺。必要になったら編集するべし
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-    public boolean onTouchEvent(MotionEvent e){
-        switch (e.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                sx = e.getX();
-                sy = e.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                break;
-            case MotionEvent.ACTION_UP:
-                tx = e.getX();
-                ty = e.getY();
-                break;
-
-        }
-
-        mainLayout.invalidate();
-
-        return super.onTouchEvent(e);
-    }
-    
+*/
 }
