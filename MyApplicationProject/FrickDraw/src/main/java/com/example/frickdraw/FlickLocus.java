0670348ac7571f13ca452c819e02373dade7acad
kino220 <m.j.1213.bad@gmail.com>
2013-10-15 09:29:46 +0900

    細かい修正

--- MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/FlickLocus.java
+++ MyApplicationProject/FrickDraw/src/main/java/com/example/frickdraw/FlickLocus.java
@@ -1,22 +1,23 @@
 package com.example.frickdraw;
 
-import android.graphics.Point;
+import android.graphics.PointF;
 
 /**
  * Created by iplab on 13/10/15.
  */
-public class FrickLocus {
+public class FlickLocus implements Cloneable {
     private int sx, sy, ex, ey;
     private long duration;
     private float vx, vy, acceleration;
-    private Point locus;
-    private float mag = 20;
-    private boolean isDrawing = false;
+    private PointF locus;
+    private float mag = 30;
     private int counter;
     private int deltime;
+    private  boolean finished;
+    private float line;
 
 
-    public FrickLocus(){
+    public FlickLocus(){
         sx = 0;
         sy = 0;
         ex = 0;
@@ -25,9 +26,13 @@ public class FrickLocus {
         vx = 0;
         vy = 0;
         acceleration = -1;
-        locus = new Point();
+        locus = new PointF();
         counter = 0;
         deltime = 30;
+        finished = false;
+        line = 0;
+
+
     }
 
     public void setSx(int sx) {
@@ -60,17 +65,25 @@ public class FrickLocus {
         this.duration = duration;
         vx = mag * (float)(Math.abs(ex - sx))/ (float)duration;
         vy = mag * (float)(Math.abs(ey - sy)) / (float)duration;
+
+
+
         counter = 0;
     }
 
-    public boolean isDrawing() {
-        return isDrawing;
+
+    public  boolean isFinished(){
+        return  finished;
     }
 
-    public Point deriveLocus(){
-        isDrawing = true;
-        vx += acceleration;
-        vy += acceleration;
+    public PointF deriveLocus(){
+
+        float l = (float) Math.sqrt((ex - sx ) * (ex - sx) + (ey - sy) * (ey - sy));
+        float cos = Math.abs((ex - sx) / l);
+        float sin = Math.abs((ey - sy) / l);
+
+        vx += acceleration * cos;
+        vy += acceleration * sin;
 
         if(vx < 0)  vx = 0;
         if(vy < 0)  vy = 0;
@@ -88,7 +101,7 @@ public class FrickLocus {
         }
 
         if(vx == 0 && vy == 0) counter++;
-        if(counter > deltime) isDrawing = false;
+        if(counter > deltime) finished = true;
 
         return  locus;
     }
